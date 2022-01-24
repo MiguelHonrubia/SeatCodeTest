eb6d432162567642111fa14841a362e1223d743f
miguelhonrubia <almihonrubia@gmail.com>
2022-01-24 18:40:05 +0100

    folder structure and db initialization

--- react-code-test/src/domain/App.tsx
+++ react-code-test/src/domain/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import "./assets/styles/App.css";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn React
         </a>
       </header>
     </div>
